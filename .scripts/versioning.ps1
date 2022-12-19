218a290e10ce1c51b4bdb6c0573561e4f23f0877
Ádám Kovács <kovacsadam07@outlook.hu>
2022-12-19 19:15:44 +0100

    Get version from tag

--- .scripts/versioning.ps1
+++ .scripts/versioning.ps1
@@ -1,1 +1,15 @@
 gci env:\
+
+if((Get-Content env:\GITHUB_REF_TYPE) -ne "tag")
+{
+    Return
+}
+
+$version = (Get-Content env:\GITHUB_REF).Replace("refs/tags/v", "")
+$git_sha = (Get-Content env:\GITHUB_SHA).Substring(0, 8)
+
+Write-Host $version
+Write-Host $git_sha
+
+(Get-Content src\Alma\Alma.csproj).Replace("0.0.0", $version).Replace("development", $git_sha) | Set-Content src\Alma\Alma.csproj
+Write-Host (Get-Content src\Alma\Alma.csproj)
--- .scripts/versioning.sh
+++ .scripts/versioning.sh
@@ -1,1 +1,16 @@
 printenv
+
+if [ ${GITHUB_REF_TYPE}  != "tag" ]; then
+    exit 1
+fi
+
+version="${GITHUB_REF:11}"
+git_hash="${GITHUB_SHA}"
+echo $git_hash
+git_hash=`expr substr $git_hash 1 8`
+
+echo $version
+echo $git_hash
+
+sed -i "s/0.0.0/$version/g;s/development/$git_hash/g" src/Alma/Alma.csproj
+cat src/Alma/Alma.csproj
